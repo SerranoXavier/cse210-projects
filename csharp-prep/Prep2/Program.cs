6c331abca53734d5d39ec25c24c930053dad9dfd
Xavier Serano <xav.serrano@yahoo.fr>
2023-01-04 22:59:26 -0500

    Assigments for Week 01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade (%): ");
+        string userInput = Console.ReadLine();
+        Console.WriteLine();
+        int grade = int.Parse(userInput);
+        int remainder = grade % 10;
+
+        string letter;
+        string sign;
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (remainder >= 7 && (letter == "A" || letter == "F"))
+        {
+            sign = "";
+        }
+        else if (remainder < 3 && letter == "F")
+        {
+            sign = "";
+        }
+        else if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{sign}.");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You did pass the class. Congratulation!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Continue on working hard, next time you will succeed.");
+        }
+
     }
 }
