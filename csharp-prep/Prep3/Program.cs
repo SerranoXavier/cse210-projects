3bee8eb010f5cbe778d6befc3a9457c8b93e0b78
Xavier Serano <xav.serrano@yahoo.fr>
2023-01-10 23:36:48 -0500

    Week 02 Prove assignments.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int tries = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userInputGuess = Console.ReadLine();
+                guess = int.Parse(userInputGuess);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    Console.WriteLine();
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    Console.WriteLine();
+                }
+                else if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine();
+                }
+
+                tries ++;
+
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"You guessed the magic number in {tries} tries.");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Do you want to play again (yes/no)? ");
+                playAgain = Console.ReadLine();
+            } while (!(playAgain == "yes" || playAgain == "y" || playAgain == "no" || playAgain == "n"));
+        } while (playAgain == "yes" || playAgain == "y");
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userInput;
+        int numberInput;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine();
+        do
+        {
+            Console.Write("Enter a number: ");
+            userInput = Console.ReadLine();
+            numberInput = int.Parse(userInput);
+            if (numberInput != 0)
+            {
+                numbers.Add(numberInput);
+            }
+        } while (numberInput != 0);
+
+        Console.WriteLine();
+
+        int sum = 0;
+        int largest = -999999999;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (largest < number)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}.");
+        Console.WriteLine($"The average is: {((float)sum)/numbers.Count}.");
+        Console.WriteLine($"The largest number is: {largest}.");
     }
 }
