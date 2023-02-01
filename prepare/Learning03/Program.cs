2ba97525da8b184f232e20ac98e4872ec9ad8be8
Xavier Serano <xav.serrano@yahoo.fr>
2023-02-01 18:28:04 -0500

    Prepare lesson - Week 05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6,7);
+
+        Console.WriteLine(first.GetTop());
+        Console.WriteLine(first.GetBottom());
+        Console.WriteLine(second.GetTop());
+        Console.WriteLine(second.GetBottom());
+        Console.WriteLine(third.GetTop());
+        Console.WriteLine(third.GetBottom());
+
+        first.SetTop(2);
+        first.SetBottom(2);
+        second.SetTop(7);
+        second.SetBottom(2);
+        third.SetTop(7);
+        third.SetBottom(8);
+
+        Console.WriteLine();
+        Console.WriteLine(first.GetTop());
+        Console.WriteLine(first.GetBottom());
+        Console.WriteLine(second.GetTop());
+        Console.WriteLine(second.GetBottom());
+        Console.WriteLine(third.GetTop());
+        Console.WriteLine(third.GetBottom());
+
+        Console.WriteLine();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(third.GetFractionString());
+
+        Console.WriteLine();
+        Console.WriteLine(first.GetDecimalValue());
+        Console.WriteLine(second.GetDecimalValue());
+        Console.WriteLine(third.GetDecimalValue());
     }
 }
