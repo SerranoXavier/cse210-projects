b025d5dce55235aef2d5933ba811a1eeab4b53d6
Xavier Serano <xav.serrano@yahoo.fr>
2023-02-15 00:43:07 -0500

    Week 07 prepare Learning04: Homework.

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class MathAssignment : Assignment{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems)
+        : base (studentName, topic) {
+        _textbookSection = textBookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList() {
+        return $"Section {_textbookSection}, problems {_problems}.";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Caesar", "History");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Pythagore", "Math", "7.3", "8-12");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Platon", "Phylosophy", "Why am I systematicaly wrong?");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
