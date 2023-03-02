a51682ef7581c16b0b6107e22b7c32ec08ef278e
Xavier Serano <xav.serrano@yahoo.fr>
2023-03-01 23:25:22 -0500

    Week 09 - Prepare (shapes)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 10);
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("green", 10, 5);
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("blue", 10);
+        // Console.WriteLine(circle.GetColor());
+        // Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>{square, rectangle, circle};
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
